0f6745d78b6cd5be6f190dcb94b3d4284a6b02a7
TuanNM <tuannm@bsscommerce.com>
2023-08-22 13:21:03 +0700

    update script

--- setup_sshkey.sh
+++ setup_sshkey.sh
@@ -4,9 +4,6 @@
 Color_Off=$(tput sgr0) # Text Reset
 Green=$(tput setaf 2)  # Green
 
-Green='\033[0;32m'
-Color_Off='\033[0m'
-
 if [ -f ~/.ssh/bss-wsl ]; then
   echo "${Green} SSH key is already exist ${Color_Off}"
   exit 0
@@ -14,7 +11,7 @@ fi
 
 mkdir -p ~/.ssh
 read -p "Enter your email address: " EMAIL_ADDRESS
-ssh-keygen -t ed25519 -b 4096 -C $EMAIL_ADDRESS -f ~/.ssh/bss-wsl
+ssh-keygen -t ed25519 -b 4096 -C $EMAIL_ADDRESS -f ~/.ssh/bss-wsl  -q -N ""
 eval `ssh-agent -s`
 ssh-add ~/.ssh/bss-wsl
 
