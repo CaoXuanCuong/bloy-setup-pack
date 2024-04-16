487b4cd4372c9dacbad023e08313e6e413b7ec46
TuanNM <tuannm@bsscommerce.com>
2024-04-16 17:18:02 +0700

    update setup pack

--- setup_b2b/run.sh
+++ setup_b2b/run.sh
@@ -101,7 +101,6 @@ update_env_single() {
 }
 
 setup_web_cms() {
-    source app.env
     mkdir -p temp
     cp web-cms.env temp/web-cms.env
     sed -i "s/<SHOPIFY_API_SECRET_KEY>/$SHOPIFY_API_SECRET_KEY/g" temp/web-cms.env
@@ -110,7 +109,7 @@ setup_web_cms() {
     sed -i "s/<EXTENSION_ID>/$EXTENSION_ID/g" temp/web-cms.env
     sed -i "s/<CF_ZONE_NAME>/$CF_ZONE_NAME/g" temp/web-cms.env
     sed -i "s/<n>/$DEV_SITE/g" temp/web-cms.env
-    mv temp/web-cms.env code_cms/web/.env
+    mv temp/web-cms.env $DESTINATION_FOLDER/code_cms/web/.env
 }
 
 setup_env() {
@@ -140,7 +139,6 @@ setup_env() {
     sed -i "s|<AMQP_URI>|$AMQP_URI|g" "${env_files[@]}"
 
     update_env
-    setup_web_cms
     echo "${Green}DONE: setup env for all services${Color_Off}"
 }
 
@@ -487,6 +485,7 @@ install)
     install_dependencies
     init_code
     setup_env
+    setup_web_cms
     init_db
     post_setup
     start
